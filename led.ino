24412ac0eaf6212bff34c22345f81d85af987780
vasilki <41682305+vasilki@users.noreply.github.com>
2018-07-25 16:01:32 +0300

    first commit

---
+++ led.ino
@@ -0,0 +1,47 @@
+int led = 8;
+int x = 3;
+void setup()
+
+{
+
+pinMode(led, OUTPUT);
+
+}
+
+void loop()
+
+{
+delay(200*x);
+
+digitalWrite(led, HIGH);
+
+delay(100*x);
+
+digitalWrite(led, LOW);
+
+delay(100*x);
+
+digitalWrite(led, HIGH);
+delay(100*x);
+
+digitalWrite(led, LOW);
+
+delay(100*x);
+
+delay(200*x);
+
+digitalWrite(led, HIGH);
+delay(50*x);
+digitalWrite(led, LOW);
+delay(150*x);
+
+delay(100*x);
+digitalWrite(led, HIGH);
+delay(50*x);
+digitalWrite(led, LOW);
+delay(50*x);
+digitalWrite(led, HIGH);
+delay(50*x);
+digitalWrite(led, LOW);
+
+}
